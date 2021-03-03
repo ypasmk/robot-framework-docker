994fb58ca1a45de4cc881f92da4867659b6b139e
Oshane Bailey <b4.oshany@gmail.com>
2021-03-03 03:00:29 -0500

    Allow an individual test suite to run when need (#10)
    * Allow an individual test suite to run when need
    
    Run an individual test suite if the TEST_SUITE environmental variable is set.
    
    * Update run_suite.sh

--- scripts/run_suite.sh
+++ scripts/run_suite.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-CMD="robot --console verbose --outputdir /reports /suites"
+
+# Run an individual test suite if the TEST_SUITE environmental variable is set.
+if [ -z "$TEST_SUITE" ]; then
+    TEST_SUITE=""
+fi
+
+CMD="robot --console verbose --outputdir /reports /suites/$TEST_SUITE"
 
 echo ${CMD}
 
